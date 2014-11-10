3c2429e0c6f7472c25d8bdd58fe4f52f860602ae
Gabe Sorensen <gabriel.k.sorensen@gmail.com>
2014-11-10 13:45:31 -0700

    GPIOClass

---
+++ pi/GPIOClass.h
@@ -0,0 +1,42 @@
+/*
+ * g.h
+ *
+ *  Created on: Nov 10, 2014
+ *      Author: Gabriellaptop
+ */
+
+#ifndef G_H_
+#define G_H_
+
+class g {
+public:
+	g();
+	virtual ~g();
+};
+
+#endif /* G_H_ */
+#ifndef GPIO_CLASS_H
+#define GPIO_CLASS_H
+
+#include <string>
+using namespace std;
+/* GPIO Class
+ * Purpose: Each object instantiated from this class will control a GPIO pin
+ * The GPIO pin number must be passed to the overloaded class constructor
+ */
+class GPIOClass
+{
+public:
+    GPIOClass();  // create a GPIO object that controls GPIO4 (default
+    GPIOClass(string x); // create a GPIO object that controls GPIOx, where x is passed to this constructor
+    int export_gpio(); // exports GPIO
+    int unexport_gpio(); // unexport GPIO
+    int setdir_gpio(string dir); // Set GPIO Direction
+    int setval_gpio(string val); // Set GPIO Value (putput pins)
+    int getval_gpio(string& val); // Get GPIO Value (input/ output pins)
+    string get_gpionum(); // return the GPIO number associated with the instance of an object
+private:
+    string gpionum; // GPIO number associated with the instance of an object
+};
+
+#endif
