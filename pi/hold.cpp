eaade27d7c498d1847bc48b7edae2dd923d50e93
Gabe Sorensen <gabriel.k.sorensen@gmail.com>
2014-11-17 22:08:19 -0700

    try3

--- pi/hold.cpp
+++ pi/hold.cpp
@@ -6,25 +6,23 @@ using namespace std;
 
 
 int main () {
-	GPIOClass in1, in2;
+	GPIOClass* in1 = new GPIOClass("4");
+	GPIOClass* in2 = new GPIOClass("17");
 	string val1, val2;
 
-	in1.GPIOClass("4");
-	in2.GPIOClass("17");
+	in1->export_gpio();
+	in2->export_gpio();
 
-	in1.export_gpio();
-	in2.export_gpio();
-
-	in1.setdir_gpio("in");
-	in2.setdir_gpio("in");
+	in1->setdir_gpio("in");
+	in2->setdir_gpio("in");
 
 	cout << "Pins are setup." << endl;
 
 
 	while (1) {
 		usleep(5000000); // 0.5 sec
-		in1.getval_gpio(val1);
-		in2.getval_gpio(val2);
+		in1->getval_gpio(val1);
+		in2->getval_gpio(val2);
 		cout << "Value of pin4: " << val1 << endl;
 		cout << "Value of pin17: " << val2 << endl;
 
