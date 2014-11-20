3b7a682c24ab58c7c5215c4109cd382045f55612
chaz2x4 <chaz2x4@users.noreply.github.com>
2014-11-19 22:19:18 -0700

    Update pwm.cpp

--- pi/pwm.cpp
+++ pi/pwm.cpp
@@ -54,12 +54,7 @@ int main (int argc, char *argv[]) {
 		double t = time_to_complete;
 		while (clock() < finish) {
 			// pulse for however long we need to to achieve brightness.
-			if(t>0){
-				t -= 1;
-			}
-			else{
-				t += 1;
-			}
+			t -= 1;
 			//Pulse(out1, sin((2PI/2) * abs(1/t)));
 			//Wait(sin((PI/2) * abs(1/t)));
 			cout << sin((2*PI)/abs(t)) << endl;
